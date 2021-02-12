1d75f9a5a9ec38344fcb3f8d655c19d4a71d8e68
Jeremy Zongker <jeremy@trilitech.com>
2021-02-12 06:34:15 +0000

    Allowed for hosting on subfolders

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import {Routing} from "./Routing";
+import { Routing } from "./Routing";
+
+const getBasename = (path: string) => path.substr(0, path.lastIndexOf('/'));
 
 const App: React.FC = () => {
   return (
-    <Router>
+    <Router basename={getBasename(window.location.pathname)}>
       <Routing />
     </Router>
   );
