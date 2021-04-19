56d1ae5eb451027beee962eb120ef628059268cc
Jared Martin <jared@livechurchsolutions.org>
2021-04-19 15:25:58 -0500

    Update github link to ChumsApp (#3)

--- src/components/HomeAbout.tsx
+++ src/components/HomeAbout.tsx
@@ -14,7 +14,7 @@ export const HomeAbout: React.FC = () => {
                             Every year the Church spends over <b>$800 million</b> on church management software.
                             We believe that money can be put to better use, which is why we developed CHUMS; a completely free, open-source church management platform.
                             We are a <b>501(c)(3) non profit</b> providing a free alternative to other paid church management platforms.
-                            Visit our <a href="https://github.com/LiveChurchSolutions/Chums" target="_blank" rel="noopener noreferrer" >GitHub</a> page to host CHUMS yourself, or let us host it for you by registering for a free account below.
+                            Visit our <a href="https://github.com/LiveChurchSolutions/ChumsApp" target="_blank" rel="noopener noreferrer" >GitHub</a> page to host CHUMS yourself, or let us host it for you by registering for a free account below.
                         </p>
                         <p>CHUMS is built and provided free of charge by <a href="https://livecs.org/">Live Church Solutions</a>, a 501(c)(3) that was founded in 2012 with the goal of helping small churches with their technical needs.</p>
                         <Button variant="light" href="https://livecs.org/" >Learn More</Button>
