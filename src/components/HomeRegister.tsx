c07c216a6cc68e0d0eb22ca835697db1702005ce
Jeremy Zongker <jeremy@trilitech.com>
2021-08-10 13:28:37 -0500

    Validate email (#15)
    * Removed password field from registration form.
    
    * Updated app base and added welcome message after register
    
    * Updated appBase

--- src/components/HomeRegister.tsx
+++ src/components/HomeRegister.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ApiHelper, RegisterInterface, LoginResponseInterface, ErrorMessages, EnvironmentHelper, PasswordField } from ".";
+import { ApiHelper, RegisterInterface, LoginResponseInterface, ErrorMessages, EnvironmentHelper } from ".";
 import { Row, Col, Container, Button, Form } from "react-bootstrap"
 import { PersonInterface } from "../helpers/Interfaces";
 import * as yup from "yup";
@@ -9,7 +9,6 @@ const schema = yup.object().shape({
   churchName: yup.string().required("Please enter your church name."),
   firstName: yup.string().required("Please enter your first name."),
   lastName: yup.string().required("Please enter your last name."),
-  password: yup.string().required("Please enter a password.").min(6, "Passwords must be at least 6 characters."),
   email: yup.string().required("Please enter your email address.").email("Please enter a valid email address.")
 })
 
@@ -39,16 +38,10 @@ export function HomeRegister() {
   async function handleSubmit(values: RegisterInterface, { setSubmitting, setErrors }: FormikHelpers<RegisterInterface>) {
     // check if user already exist with this email.
     setCustomErrors([]);
-    const verifyResponse = await ApiHelper.postAnonymous("/users/verifyCredentials", {email: values.email, password: values.password}, "AccessApi");
-    if (verifyResponse.errors || verifyResponse.churches) {
-      const errorMessage = <>There is already an account with this email address, please <a href={EnvironmentHelper.AccountsAppUrl}>login</a> to manage your churches and apps. If you wish to create a new church with this email, please register via <a href={EnvironmentHelper.ChurchAppUrl}>ChurchApps</a></>;
-      setCustomErrors([errorMessage]);
 
-      return;
-    }
     const loginResp = await createAccess(values);
     if (loginResp != null) {
-      const { person }: { person: PersonInterface} = await ApiHelper.post("/churches/init", { user: loginResp.user }, "MembershipApi");
+      const { person }: { person: PersonInterface } = await ApiHelper.post("/churches/init", { user: loginResp.user }, "MembershipApi");
       const promises: Promise<any>[] = [];
       promises.push(ApiHelper.post("/userchurch", { personId: person.id }, "AccessApi"));
       promises.push(ApiHelper.post("/churches/init", [], "AttendanceApi"));
@@ -58,13 +51,13 @@ export function HomeRegister() {
       let errors: string[] = undefined;
       responses.forEach(resp => { if (resp.errors !== undefined) errors = resp.errors; });
       if (errors !== undefined) { setCustomErrors(errors); return 0; }
-      else setRedirectUrl(EnvironmentHelper.AppUrl);
+      else setRedirectUrl(EnvironmentHelper.AppUrl + "/login?checkEmail=1");
 
     }
     setSubmitting(false);
   }
 
-  const initialValues: RegisterInterface = { churchName: "", firstName: "", lastName: "", password: "", email: "" };
+  const initialValues: RegisterInterface = { churchName: "", firstName: "", lastName: "", email: "", appName: "Chums", appUrl: EnvironmentHelper.AppUrl };
 
   if (redirectUrl === "") {
     return (
@@ -80,83 +73,24 @@ export function HomeRegister() {
               <p>You'll be up and running in less than a minute.</p>
 
               <ErrorMessages errors={customErrors} />
-              <Formik
-                validationSchema={schema}
-                onSubmit={handleSubmit}
-                initialValues={initialValues}
-              >
-                {({
-                  handleSubmit,
-                  handleChange,
-                  values,
-                  touched,
-                  errors,
-                  isSubmitting
-                }) => (
+              <Formik validationSchema={schema} onSubmit={handleSubmit} initialValues={initialValues}>
+                {({ handleSubmit, handleChange, values, touched, errors, isSubmitting }) => (
                   <Form noValidate onSubmit={handleSubmit}>
                     <Form.Group>
-                      <Form.Control
-                        type="text"
-                        placeholder="Church Name"
-                        name="churchName"
-                        value={values.churchName}
-                        onChange={handleChange}
-                        isInvalid={touched.churchName && !!errors.churchName}
-                      />
-
-                      <Form.Control.Feedback type="invalid">
-                        {errors.churchName}
-                      </Form.Control.Feedback>
+                      <Form.Control type="text" placeholder="Church Name" name="churchName" value={values.churchName} onChange={handleChange} isInvalid={touched.churchName && !!errors.churchName} />
+                      <Form.Control.Feedback type="invalid">{errors.churchName}</Form.Control.Feedback>
                     </Form.Group>
                     <Form.Group>
-                      <Form.Control
-                        type="text"
-                        placeholder="First Name"
-                        name="firstName"
-                        value={values.firstName}
-                        onChange={handleChange}
-                        isInvalid={touched.firstName && !!errors.firstName}
-                      />
-
-                      <Form.Control.Feedback type="invalid">
-                        {errors.firstName}
-                      </Form.Control.Feedback>
+                      <Form.Control type="text" placeholder="First Name" name="firstName" value={values.firstName} onChange={handleChange} isInvalid={touched.firstName && !!errors.firstName} />
+                      <Form.Control.Feedback type="invalid">{errors.firstName}</Form.Control.Feedback>
                     </Form.Group>
                     <Form.Group>
-                      <Form.Control
-                        type="text"
-                        placeholder="Last Name"
-                        name="lastName"
-                        value={values.lastName}
-                        onChange={handleChange}
-                        isInvalid={touched.lastName && !!errors.lastName}
-                      />
-
-                      <Form.Control.Feedback type="invalid">
-                        {errors.lastName}
-                      </Form.Control.Feedback>
+                      <Form.Control type="text" placeholder="Last Name" name="lastName" value={values.lastName} onChange={handleChange} isInvalid={touched.lastName && !!errors.lastName} />
+                      <Form.Control.Feedback type="invalid">{errors.lastName}</Form.Control.Feedback>
                     </Form.Group>
                     <Form.Group>
-                      <Form.Control
-                        type="email"
-                        placeholder="Email"
-                        name="email"
-                        value={values.email}
-                        onChange={handleChange}
-                        isInvalid={touched.email && !!errors.email}
-                      />
-
-                      <Form.Control.Feedback type="invalid">
-                        {errors.email}
-                      </Form.Control.Feedback>
-                    </Form.Group>
-                    <Form.Group>
-                      <PasswordField
-                        value={values.password}
-                        onChange={handleChange}
-                        isInvalid={touched.password && !!errors.password}
-                        errorText={errors.password}
-                      />
+                      <Form.Control type="email" placeholder="Email" name="email" value={values.email} onChange={handleChange} isInvalid={touched.email && !!errors.email} />
+                      <Form.Control.Feedback type="invalid">{errors.email}</Form.Control.Feedback>
                     </Form.Group>
                     <Button type="submit" variant="success" block disabled={isSubmitting}>
                       {isSubmitting ? "Registering. Please wait..." : "Get Started for Free"}
