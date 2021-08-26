d0126c34e8c59bf244226909fbfba9dbb70b7bd2
Jeremy Zongker <jeremy@trilitech.com>
2021-08-26 14:32:33 +0000

    Updated app base and removed unused css

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./Home.css";
 import { Footer, Header } from "./components"
 import { HomeRegister } from "./components/HomeRegister"
 import { HomeHero } from "./components/HomeHero"
