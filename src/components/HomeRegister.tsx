7213b2ad750c35069d86378e3cb11cf7b5d8e65a
Jeremy Zongker <jeremy@trilitech.com>
2021-08-27 18:53:26 +0000

    Added analytics tracking for register

--- src/components/HomeRegister.tsx
+++ src/components/HomeRegister.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import { ErrorMessages, EnvironmentHelper } from ".";
 import { Row, Col, Container } from "react-bootstrap"
 import { Register } from "../appBase/pageComponents/components/Register"
+import ReactGA from "react-ga";
+import { UserInterface } from "../appBase/interfaces";
 
 export function HomeRegister() {
   const [customErrors, setCustomErrors] = React.useState<string[]>([]);
+
+  const trackRegister = async (user: UserInterface) => {
+    if (EnvironmentHelper.GoogleAnalyticsTag !== "") ReactGA.event({ category: "User", action: "Register" });
+  }
 
   return (
     <div className="homeSection" id="registerSection">
@@ -19,7 +25,7 @@ export function HomeRegister() {
             <p>You'll be up and running in less than a minute.</p>
 
             <ErrorMessages errors={customErrors} />
-            <Register updateErrors={setCustomErrors} appName="ChurchApps" appUrl={EnvironmentHelper.AccountsAppUrl} />
+            <Register updateErrors={setCustomErrors} appName="ChurchApps" appUrl={EnvironmentHelper.AccountsAppUrl} userRegisteredCallback={trackRegister} />
           </Col>
         </Row>
       </Container>
