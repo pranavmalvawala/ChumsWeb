fe5dd4dfe1254cf2a9d907f7267eec731e2a9427
Jeremy Zongker <jeremy@trilitech.com>
2022-01-03 13:57:53 -0600

    Updated packages

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Home } from "./Home";
 import { Privacy } from "./Privacy";
 import { Terms } from "./Terms";
-import { Switch, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import ReactGA from "react-ga";
 import { EnvironmentHelper } from "./helpers";
 
@@ -15,10 +15,10 @@ export const Routing: React.FC = () => {
   React.useEffect(() => { if (EnvironmentHelper.GoogleAnalyticsTag !== "") ReactGA.pageview(location.pathname + location.search); }, [location]);
 
   return (
-    <Switch>
-      <Route path="/privacy"><Privacy /></Route>
-      <Route path="/terms"><Terms /></Route>
-      <Route path="/"><Home /></Route>
-    </Switch>
+    <Routes>
+      <Route path="/privacy" element={<Privacy />} />
+      <Route path="/terms" element={<Terms />} />
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
